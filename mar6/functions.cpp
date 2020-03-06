2b0c69579ff451907cfb173e09bd9349328b5f7d
Si Le <omnisyle@gmail.com>
2020-03-06 17:43:57 -0500

    adds the rest of the questions

--- mar6/functions.cpp
+++ mar6/functions.cpp
@@ -39,7 +39,10 @@ int sumDigits(int n) {
 }
 
 void Question2() {
-  cout << sumDigits(123) << endl;
+  int num;
+  cout << "Enter a number to find the digit sum for: ";
+  cin >> num;
+  cout << "The sum of the digits in " << num << " is " << sumDigits(num) << endl;
 }
 
 int reverse(int number) {
@@ -154,8 +157,164 @@ void Question7(){
   futureInvestmentValue(investmenAmt, annualInterestRate, 30);
 }
 
-void Question8(){}
-void Question9(){}
-void Question10(){}
-void Question11(){}
-void Question12(){}
+void printMatrix(int n) {
+  srand(time(0));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      int oneOrZero = rand()%2;
+      cout << oneOrZero << setw(8);
+    }
+    cout << setw(0);
+    cout << endl;
+  }
+}
+void Question8(){
+  int num;
+  cout << "Enter a number for matrix: ";
+  cin >> num;
+  printMatrix(num);
+}
+
+double distance(int x1, int y1, int x2, int y2) {
+  return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+}
+
+void Question9(){
+  int x1;
+  int x2;
+  int y1;
+  int y2;
+
+  cout << "Enter x1: ";
+  cin >> x1;
+  cout << "Enter y1: ";
+  cin >> y1;
+  cout << "Enter x2: ";
+  cin >> x2;
+  cout << "Enter y2: ";
+  cin >> y2;
+  cout << "The distance is " << distance(x1, y1, x2, y2) << endl;
+}
+
+void convertMillis(int millis)
+{
+  int seconds = millis / 1000 % 60;
+  int minutes = ((millis / (1000*60)) % 60);
+  int hours = (millis / 1000)/60/60;
+
+  cout << hours << ":" << minutes << ":" << seconds << endl;
+}
+
+void Question10()
+{
+  int millis;
+  cout << "Enter millisecond: ";
+  cin >> millis;
+  convertMillis(millis);
+}
+
+void addCoins(int pennies, int nickels, int dimes, int quarters)
+{
+  int nickelsInPennies =  nickels * 5;
+  int dimesInPennies = dimes * 10;
+  int quartersInPennies = quarters * 25;
+
+  int totalPennies = pennies + nickelsInPennies + dimesInPennies + quartersInPennies;
+  double total = totalPennies / 100.00;
+  cout << "Total: $" << total << endl;
+}
+
+void Question11()
+{
+  int quarters;
+  int pennies;
+  int nickels;
+  int dimes;
+
+  cout << "Enter your total coins: " << endl;
+  cout << endl;
+  cout << "Quarters: ";
+  cin >> quarters;
+  cout << "Dimes: ";
+  cin >> dimes;
+  cout << "Nickels: ";
+  cin >> nickels;
+  cout << "Pennies: ";
+  cin >> pennies;
+
+  cout << endl;
+  addCoins(pennies, nickels, dimes, quarters);
+}
+
+void printMessage() {
+  cout << "Please choose an option (1-12 or -1 to Quit)" << endl;
+  cout << "1. Pentagonal numbers" << endl;
+  cout << "2. Digit sum" << endl;
+  cout << "3. Reverse a number" << endl;
+  cout << "4. Palindrom application" << endl;
+  cout << "5. Display sorted number " << endl;
+  cout << "6. Display pattern" << endl;
+  cout << "7. Future Investment" << endl;
+  cout << "8. Print matrix" << endl;
+  cout << "9. Calculate distance" << endl;
+  cout << "10. Convert millis" << endl;
+  cout << "11. Add Coins" << endl;
+  cout << "12. Enter -1 to Quit" << endl;
+}
+
+void Question12()
+{
+  cout << "**********************************************" << endl;
+  cout << "Welcome to user-defined functions assignments" << endl;
+  cout << "**********************************************" << endl;
+  printMessage();
+  int option = 0;
+  while (option > -1) {
+    cout << "Enter the function number to test: ";
+    cin >> option;
+    switch (option)
+    {
+    case 1:
+      Question1();
+      break;
+    case 2:
+      Question2();
+      break;
+    case 3:
+      Question3();
+      break;
+    case 4:
+      Question4();
+      break;
+    case 5:
+      Question5();
+      break;
+    case 6:
+      Question6();
+      break;
+    case 7:
+      Question7();
+      break;
+    case 8:
+      Question8();
+      break;
+    case 9:
+      Question9();
+      break;
+    case 10:
+      Question10();
+      break;
+    case 11:
+      Question11();
+      break;
+    case 12:
+      Question12();
+      break;
+    case -1:
+      break;
+    default:
+      break;
+    }
+  }
+  cout << "Thank you for using the functions module." << endl;
+}
